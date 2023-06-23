e5000cab3cf6b23ce52ac251adee143d2e32af73
swickape <patrick.swickard@amplyfigroup.com>
2023-06-23 10:35:46 -0400

    move stanza analysis into class

--- analyze.py
+++ analyze.py
@@ -82,76 +82,11 @@ def get_letter_frequencies(thispoem):
     print(letter_count)
     print(letters_sorted)
 
-def get_firstline(thispoem):
-  lines = thispoem.lines
-  if lines[0] and not lines[1] and lines[2]:
-    title = lines[0]
-    dedicatee = ''
-    firstline = 2
-  elif lines[0] and lines[1] and not lines[2] and lines[3]:
-    title = lines[0]
-    dedicatee = lines[1]
-    firstline = 3
-  else:
-    raise 'This does not fit the format'
-  return [title, dedicatee, firstline]
-
-def get_stanzas(thispoem):
-  lines = thispoem.lines
-  stanza_count = 0
-  stanza_length = 0
-  line_count = 0
-  stanzas = []
-  in_stanza = False
-  firstvals = get_firstline(thispoem)
-  title = firstvals[0]
-  dedicatee = firstvals[1]
-  firstline = firstvals[2]
-  poem_text = lines
-  for thisline in poem_text[firstline:]:
-    if thisline:
-      if in_stanza:
-        in_stanza = True
-        stanza_length += 1
-        line_count += 1
-      else:
-        in_stanza = True
-        stanza_length += 1
-        line_count += 1
-    else:
-      if in_stanza:
-        in_stanza = False
-        stanzas.append(stanza_length)
-        stanza_length = 0
-      else:
-        in_stanza = False
-  # one more stanza check at end
-  if in_stanza:
-    stanzas.append(stanza_length)
-  else:
-    pass
-  stanza_count = len(stanzas)
-  nonempty_lines = []
-  for thisline in lines:
-    if thisline:
-      nonempty_lines.append(thisline)
-  unique_lengths = numpy.unique(stanzas)
-
-  if stanza_count == 1:
-    print('MYCOUNTS,' + str(line_count) + ',' + '1' + ',' + str(line_count))
-  else:
-    if len(unique_lengths) == 1:
-      print('MYCOUNTS,' + str(line_count) + ',' + str(stanza_count) + ',' + str(stanzas[0]))
-    else:
-#      print('The poem "' + title + '" consists of ' + str(stanza_count) + ' stanzas of irregular length.'  + ': ' + str(line_count) + ' total.')
-      print('MYCOUNTS,' + str(line_count) + ',' + str(stanza_count) + ',' + '-')
-
-
 def analyze_poem(thispoem):
-  get_stanzas(thispoem)
-  get_letter_frequencies(thispoem)
-  get_word_frequencies(thispoem)
-  get_syllable_data(thispoem)
+  thispoem.get_stanzas()
+  #get_letter_frequencies(thispoem)
+  #get_word_frequencies(thispoem)
+  #get_syllable_data(thispoem)
 
 def read_poem(poem_file):
   with open(poem_file) as fd:
@@ -175,6 +110,7 @@ values = range(40)
 for i in values:
   poem_file = '/home/swickape/projects/github/plathagrams/spsidebyside' + str(i+1) + '.txt'
   this_poem = read_poem(poem_file)
+  print('HOOHAH' + str(this_poem.length()))
   analyze_poem(this_poem)
 for i in values:
   poem_file = '/home/swickape/projects/github/plathagrams/anagram_' + str(i+1) + '.txt'
--- poemstruct.py
+++ poemstruct.py
@@ -1,3 +1,5 @@
+import numpy
+
 class Book:
   def __init__(self):
     self.title = ''
@@ -9,3 +11,70 @@ class Poem:
     self.title = title
     self.dedicatee = dedicatee
     self.lines = lines
+
+  def length(self):
+    return len(self.lines)
+
+  def get_firstline(self):
+    lines = self.lines
+    if lines[0] and not lines[1] and lines[2]:
+      title = lines[0]
+      dedicatee = ''
+      firstline = 2
+    elif lines[0] and lines[1] and not lines[2] and lines[3]:
+      title = lines[0]
+      dedicatee = lines[1]
+      firstline = 3
+    else:
+      raise 'This does not fit the format'
+    return [title, dedicatee, firstline]
+
+  def get_stanzas(self):
+    lines = self.lines
+    stanza_count = 0
+    stanza_length = 0
+    line_count = 0
+    stanzas = []
+    in_stanza = False
+    firstvals = self.get_firstline()
+    title = firstvals[0]
+    dedicatee = firstvals[1]
+    firstline = firstvals[2]
+    poem_text = lines
+    for thisline in poem_text[firstline:]:
+      if thisline:
+        if in_stanza:
+          in_stanza = True
+          stanza_length += 1
+          line_count += 1
+        else:
+          in_stanza = True
+          stanza_length += 1
+          line_count += 1
+      else:
+        if in_stanza:
+          in_stanza = False
+          stanzas.append(stanza_length)
+          stanza_length = 0
+        else:
+          in_stanza = False
+    # one more stanza check at end
+    if in_stanza:
+      stanzas.append(stanza_length)
+    else:
+      pass
+    stanza_count = len(stanzas)
+    nonempty_lines = []
+    for thisline in lines:
+      if thisline:
+        nonempty_lines.append(thisline)
+    unique_lengths = numpy.unique(stanzas)
+
+    if stanza_count == 1:
+      print('MYCOUNTS,' + str(line_count) + ',' + '1' + ',' + str(line_count))
+    else:
+      if len(unique_lengths) == 1:
+        print('MYCOUNTS,' + str(line_count) + ',' + str(stanza_count) + ',' + str(stanzas[0]))
+      else:
+#        print('The poem "' + title + '" consists of ' + str(stanza_count) + ' stanzas of irregular length.'  + ': ' + str(line_count) + ' total.')
+        print('MYCOUNTS,' + str(line_count) + ',' + str(stanza_count) + ',' + '-')
