e5e24ab66077c259c272fc172ba00757ad38df83
swickape <patrick.swickard@amplyfigroup.com>
2023-07-02 12:42:24 -0400

    new analyze to json, cleanup, checkin json data for poems

--- analyze.py
+++ analyze.py
@@ -7,11 +7,17 @@ def analyze_poem(thispoem):
   print('*********************')
   print('I read a poem today!')
   print(thispoem.title)
+  print()
   thispoem.print_poem_stanza_info()
   thispoem.print_poem_syllable_info()
   thispoem.print_line_info()
   thispoem.print_poem_word_frequencies()
   thispoem.print_poem_letter_frequencies()
+
+def analyze_poem_to_json(thispoem,filename):
+  f = open(filename,'w')
+  f.write(thispoem.dump_poem_hash())
+  f.close()
 
 def read_poem(poem_file):
   with open(poem_file) as fd:
@@ -19,40 +25,69 @@ def read_poem(poem_file):
     thispoem = poemstruct.Poem(lines)
     return thispoem
 
-corpus_word_frequency_plath = {}
-corpus_word_frequency_sylph = {}
+def analyze_corpus_to_json():
+  corpus_word_frequency_plath = {}
+  corpus_word_frequency_sylph = {}
 
-values = range(40)
-for i in values:
-  poem_file = '/home/swickape/projects/github/plathagrams/spsidebyside' + str(i+1) + '.txt'
-  this_poem = read_poem(poem_file)
-  analyze_poem(this_poem)
-  for thiskey,thisvalue in this_poem.poem_word_frequencies_hash.items():
-    if corpus_word_frequency_plath.get(thiskey):
-      corpus_word_frequency_plath[thiskey] = corpus_word_frequency_plath[thiskey] + thisvalue
+  values = range(40)
+  for i in values:
+    poem_file = '/home/swickape/projects/github/plathagrams/spsidebyside' + str(i+1) + '.txt'
+    this_poem = read_poem(poem_file)
+    if i < 10:
+      filename = 'sylph/poem_0' + str(i) + '.json'
     else:
-      corpus_word_frequency_plath[thiskey] = thisvalue
-for i in values:
-  poem_file = '/home/swickape/projects/github/plathagrams/anagram_' + str(i+1) + '.txt'
-  this_poem = read_poem(poem_file)
-  print(str(this_poem.length()))
-  analyze_poem(this_poem)
-  print('word hash for ' + this_poem.title + ':')
-  print(this_poem.poem_word_frequencies_hash)
-  for thiskey,thisvalue in this_poem.poem_word_frequencies_hash.items():
-    if corpus_word_frequency_sylph.get(thiskey):
-      corpus_word_frequency_sylph[thiskey] = corpus_word_frequency_sylph[thiskey] + thisvalue
+      filename = 'sylph/poem_' + str(i) + '.json'
+    analyze_poem_to_json(this_poem,filename)
+    for thiskey,thisvalue in this_poem.poem_word_frequencies_hash.items():
+      if corpus_word_frequency_plath.get(thiskey):
+        corpus_word_frequency_plath[thiskey] = corpus_word_frequency_plath[thiskey] + thisvalue
+      else:
+        corpus_word_frequency_plath[thiskey] = thisvalue
+  for i in values:
+    poem_file = '/home/swickape/projects/github/plathagrams/anagram_' + str(i+1) + '.txt'
+    if i < 10:
+      filename = 'plath/poem_0' + str(i) + '.json'
     else:
-      corpus_word_frequency_sylph[thiskey] = thisvalue
+      filename = 'plath/poem_' + str(i) + '.json'
+    analyze_poem_to_json(this_poem,filename)
 
-print('*********************')
-print('Plath corpus:')
-#print(corpus_word_frequency_plath)
-sorted_corpus_word_frequency_plath = sorted(corpus_word_frequency_plath.items(), key=lambda x:x[1],reverse=True)
-print(sorted_corpus_word_frequency_plath)
-print('*********************')
-print('Sylph corpus:')
-#print(corpus_word_frequency_sylph)
-sorted_corpus_word_frequency_sylph = sorted(corpus_word_frequency_sylph.items(), key=lambda x:x[1],reverse=True)
-print(sorted_corpus_word_frequency_sylph)
-print('*********************')
+
+def analyze_corpus():
+  corpus_word_frequency_plath = {}
+  corpus_word_frequency_sylph = {}
+
+  values = range(40)
+  for i in values:
+    poem_file = '/home/swickape/projects/github/plathagrams/spsidebyside' + str(i+1) + '.txt'
+    this_poem = read_poem(poem_file)
+    analyze_poem(this_poem)
+    for thiskey,thisvalue in this_poem.poem_word_frequencies_hash.items():
+      if corpus_word_frequency_plath.get(thiskey):
+        corpus_word_frequency_plath[thiskey] = corpus_word_frequency_plath[thiskey] + thisvalue
+      else:
+        corpus_word_frequency_plath[thiskey] = thisvalue
+  for i in values:
+    poem_file = '/home/swickape/projects/github/plathagrams/anagram_' + str(i+1) + '.txt'
+    this_poem = read_poem(poem_file)
+    print(str(this_poem.length()))
+    analyze_poem(this_poem)
+    print('word hash for ' + this_poem.title + ':')
+    print(this_poem.poem_word_frequencies_hash)
+    for thiskey,thisvalue in this_poem.poem_word_frequencies_hash.items():
+      if corpus_word_frequency_sylph.get(thiskey):
+        corpus_word_frequency_sylph[thiskey] = corpus_word_frequency_sylph[thiskey] + thisvalue
+      else:
+        corpus_word_frequency_sylph[thiskey] = thisvalue
+
+  print('*********************')
+  print('Plath corpus:')
+  sorted_corpus_word_frequency_plath = sorted(corpus_word_frequency_plath.items(), key=lambda x:x[1],reverse=True)
+  print(sorted_corpus_word_frequency_plath)
+  print('*********************')
+  print('Sylph corpus:')
+  sorted_corpus_word_frequency_sylph = sorted(corpus_word_frequency_sylph.items(), key=lambda x:x[1],reverse=True)
+  print(sorted_corpus_word_frequency_sylph)
+  print('*********************')
+
+#analyze_corpus()
+analyze_corpus_to_json()
--- poemstruct.py
+++ poemstruct.py
@@ -20,6 +20,16 @@ class Line:
     self.sig = ''
     self.length_letters_only = 0
 
+  def build_line_hash(self):
+    thishash = {}
+    thishash['text'] = self.text
+    thishash['syllable_list'] = self.syllable_list
+    thishash['syllable_count'] = self.syllable_count
+    thishash['letters_only'] = self.letters_only
+    thishash['sig'] = self.sig
+    thishash['length_letters_only'] = 0
+    return thishash
+
 class Poem:
   def __init__(self,lines):
     self.lines = lines
@@ -41,6 +51,7 @@ class Poem:
     self.sig = ''
     self.length_letters_only = 0
     self.get_letter_frequencies()
+    poem_hash = self.build_poem_hash()
 
   def length(self):
     return len(self.lines)
@@ -100,7 +111,7 @@ class Poem:
     self.stanza_count = stanza_count
     self.stanza_unique_lengths = numpy.unique(stanzas)
     self.nonempty_line_count = nonempty_line_count
-    self.stanzas = stanzas
+    self.stanza_structure = stanzas
 
   def get_string_sig(self,string):
     letters_only = self.get_string_letters(string)
@@ -192,56 +203,80 @@ class Poem:
           number_of_syllables = sum(line_syllable_list)
           thisline.syllable_count = number_of_syllables
           total_syllables += number_of_syllables
-    perline = round(total_syllables/nonempty_line_count,3)
+    syllables_per_line = round(total_syllables/nonempty_line_count,3)
     self.total_syllables = total_syllables
     self.nonempty_line_count = nonempty_line_count
-    self.perline = perline
+    self.syllables_per_line = syllables_per_line
 
   def print_poem_syllable_info(self):
     print('1 - title, syllables, lines:' + self.title + ',' + str(self.total_syllables) + ',' + str(self.nonempty_line_count))
-    print('2 - average syllables per non-blank line:' + str(self.perline))
+    print('2 - average syllables per non-blank line:' + str(self.syllables_per_line))
 
   def print_poem_stanza_info(self):
     if self.stanza_count == 1:
       print('The poem "' + self.title + '" consists of ' + '1' + ' stanza of length' + str(self.stanza_unique_lengths) + '.'  + ': ' + str(self.nonempty_line_count) + ' total.')
     else:
       if len(self.stanza_unique_lengths) == 1:
-        print('The poem "' + self.title + '" consists of ' + str(self.stanza_count) + ' stanzas of length' + str(self.stanza_unique_lengths) + '.'  + ': ' + str(self.nonempty_line_count) + ' total.')
+        print('The poem "' + self.title + '" consists of ' + str(self.stanza_count) + ' stanzas of length' + str(self.stanza_unique_lengths) + ': ' + str(self.nonempty_line_count) + ' total.')
       else:
         print('The poem "' + self.title + '" consists of ' + str(self.stanza_count) + ' stanzas of irregular length.'  + ': ' + str(self.nonempty_line_count) + ' total.')
-    print('6: stanza structure:' + str(self.stanzas))
+    print('6: stanza structure:' + str(self.stanza_structure))
     print('\n')
 
   def print_line_letter_frequencies(self,thisline):
     if thisline.text:
-      print('Line text:' + thisline.text)
-      print('Letters:  ' + thisline.letters_only)
-      print('Sig:      ' + thisline.sig)
-      print('Line length letters only:' + str(thisline.length_letters_only))
-      print('\n')
+      print('Line text: ' + thisline.text)
+      print('Letters:    ' + thisline.letters_only)
+      print('Sig:         ' + thisline.sig)
+      print('Line length letters only: ' + str(thisline.length_letters_only))
+      print()
 
   def print_poem_letter_frequencies(self):
-    print('Poem text:' + self.text)
-    print('Letters:  ' + self.letters_only)
-    print('Sig:      ' + self.sig)
-    print('Poem length letters only:' + str(self.length_letters_only))
+    print('Poem text: ' + self.text)
+    print('Letters:    ' + self.letters_only)
+    print('Sig:         ' + self.sig)
+    print('Poem length letters only: ' + str(self.length_letters_only))
     print('\n')
 
   def print_poem_word_frequencies(self):
     print(self.poem_word_frequencies_hash)
 
   def print_line_syllable_list(self,thisline):
-    print(thisline.text)
-    if (None in thisline.syllable_list):
-      print('Word with unknown syllable count found in list.')
-      print(thisline.syllable_list)
-    else:
-      print(str(thisline.syllable_count) + ' syllables in line for recommended pronunciation')
-      print('Syllable structure:' + str(thisline.syllable_list))
-      print()
+    if thisline.text:
+      print(thisline.text)
+      if (None in thisline.syllable_list):
+        print('Word with unknown syllable count found in list.')
+        print(thisline.syllable_list)
+        print()
+      else:
+        print(str(thisline.syllable_count) + ' syllables in line for recommended pronunciation')
+        print('Syllable structure: ' + str(thisline.syllable_list))
+        print()
 
   def print_line_info(self):
     for thisline in self.linelist:
       if thisline:
         self.print_line_letter_frequencies(thisline)
         self.print_line_syllable_list(thisline)
+
+  def build_poem_hash(self):
+    thishash = {}
+    thishash['title'] = self.title
+    thishash['text'] = self.text
+    thishash['nonempty_line_count'] = self.nonempty_line_count
+    thishash['stanza_count'] = self.stanza_count
+    thishash['stanza_structure'] = self.stanza_structure
+    thishash['total_syllables'] = self.total_syllables
+    thishash['syllables_per_line'] = self.syllables_per_line
+    thishash['poem_word_frequencies_hash'] = self.poem_word_frequencies_hash
+    thishash['sig'] = self.sig
+    thishash['length_letters_only'] = self.length_letters_only
+    linelist = []
+    for thisline in self.linelist:
+      linehash = thisline.build_line_hash()
+      linelist.append(linehash)
+    thishash['linelist'] = linelist
+    return thishash
+
+  def dump_poem_hash(self):
+    return json.dumps(self.build_poem_hash())
