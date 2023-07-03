5f418fa6d38597468036690595f4c18f6e47056a
Patrick Swickard <swickape@hotmail.com>
2023-07-03 10:02:26 -0400

    multi checkin and typo fix

---
+++ multi_analyze.py
@@ -0,0 +1,34 @@
+import numpy
+import re
+import json
+import poemstruct
+import os
+
+def analyze_poem(thispoem):
+  print('*********************')
+  print('I read a poem today!')
+  print(thispoem.title)
+  print()
+  thispoem.print_poem_stanza_info()
+  thispoem.print_poem_syllable_info()
+  thispoem.print_line_info()
+  thispoem.print_poem_word_frequencies()
+  thispoem.print_poem_letter_frequencies()
+
+def analyze_poem_to_json(thispoem,filename):
+  f = open(filename,'w')
+  f.write(thispoem.dump_poem_hash())
+  f.close()
+
+def read_poem(poem_file):
+  with open(poem_file) as fd:
+    lines = fd.read().splitlines()
+    thispoem = poemstruct.Poem(lines)
+    return thispoem
+
+list_of_files = os.listdir('baudtxt2')
+for thisfile in list_of_files:
+  #print(thisfile)
+  poem_file = 'baudtxt2/' + thisfile
+  this_poem = read_poem(poem_file)
+  analyze_poem_to_json(this_poem,'outfile.json')
